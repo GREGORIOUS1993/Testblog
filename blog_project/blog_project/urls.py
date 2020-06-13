8415c8f123200ecd1a0ede3ff55e1ce5b4b33155
GREGORIOUS1993 <65975503+GREGORIOUS1993@users.noreply.github.com>
2020-06-13 01:15:00 +0000

    Update urls.py

--- blog_project/blog_project/urls.py
+++ blog_project/blog_project/urls.py
@@ -25,7 +25,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('article.urls')),
     path('authenticate/', include('authenticate.urls')),
-	path('oauth/', include('social_django.urls', namespace='social')),  # <--
+
 ]
 
 
